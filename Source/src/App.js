e3949a09c317b6d2d0c1491adc1ac5539305975e
SaifKhalifa <khsaif5@gmail.com>
2024-11-20 23:53:31 +0200

    Updated "gitignore" + fixed some issues with the home page

--- Source/src/App.js
+++ Source/src/App.js
@@ -12,16 +12,28 @@ function App() {
       <section className="py-5">
         <div className="container px-4 px-lg-5 mt-5">
           <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            <Card originalPrice = {10} discountPercent = {0.25} productTitle = "prdct1"/>
-            <Card productImageSource="https://dummyimage.com/450x300/ff08ff/0011ff" productTitle = "" originalPrice = {135000} discountPercent = {0.55} buttonText = "Let me have this product"/>
+
+            <Card 
+              originalPrice = {10} 
+              discountPercent = {0.25} 
+              productTitle = "prdct1"
+            />
+            
+            <Card 
+              productImageSource="https://dummyimage.com/450x300/ff08ff/0011ff" 
+              productTitle = "" 
+              originalPrice = {135000} 
+              discountPercent = {0.55} 
+              buttonText = "Let me have this product"
+            />
+            
             <Card src="youtubrweibfebf.com" topic="Music" class="Music"/>
+            
             <Card />
             <Card />
+            <Card />            
             <Card />
-            <div class="hidden">
-              <Card />
-            <Card />
-            </div>
+            <Card />      
             
           </div>
         </div>
